25109999b99aa47ee78af7732cdd30095d168dcc
Dmytro Vakoliuk <divanura@gmail.com>
2017-02-06 14:35:02 +0200

    feractor

--- app/public/js/components/form.js
+++ app/public/js/components/form.js
@@ -16,12 +16,11 @@ function addUser() {
             }).then((data) => {
                 let id =JSON.parse(data);
                 let row = document.createElement("TR");
-                row.innerHTML = `<td>${id.id}</td><td>${name}</td><td><a href="#delete" class="delete">Удалить</a> | <a href="#update">Изменить</a></td>`;
+                row.innerHTML = `<td>${id.id}</td><td>${name}</td><td><a href="#delete" class="delete">Удалить</a> | <a href="#update" class="update">Изменить</a></td>`;
                 document.getElementById("tableUsers").appendChild(row);
 
             }).then(() => {
                 document.getElementById("myForm").reset();
-                helpers.listeners();
             }).catch(error => {
                 console.log(error);
             });
